90900e15629811c09f81985eb1793c6064376896
charles <wkc1002@naver.com>
2023-05-17 18:01:19 +0900

    tymeleaf - spring & form

---
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -0,0 +1,83 @@
+package hello.itemservice.web.form;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/form/items")
+@RequiredArgsConstructor
+public class FormItemController {
+
+    private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        /* Controller 호출할 때마다 ModelAttribute에서 자동으로 addAttribute() 모델에 무조건 담긴다.
+         * 이름은 애너테이션의 파라미터로 들어간 "regions"로 담긴다.
+         * 어떤 컨트롤러를 호출하든지 다 자동으로 regions가 모델에 담기는 것이 보장이 된다.*/
+
+        Map<String, String> regions = new LinkedHashMap<>(); // 순서를 보장하기 위해서 LinkedHashMap 사용
+        regions.put("SEOUL", "서울"); // key : 실제 시스템에서 왔다갔다하는 값, value : 사용자에게 보여주기 위한 값
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+//        model.addAttribute("regions", regions); // 이게 자동으로 호출되는 격이다.
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "form/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
+        log.info("item.getRegions()={}", item.getRegions());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/form/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/form/items/{itemId}";
+    }
+
+}
+
